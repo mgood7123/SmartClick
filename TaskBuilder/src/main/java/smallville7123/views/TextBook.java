7a29cf04126eda92d1a89401379097751d602907
smallville7123 <smallville7123@gmail.com>
2020-10-21 14:03:14 +1000

    correctly detect line max width :)

--- TaskBuilder/src/main/java/smallville7123/views/TextBook.java
+++ TaskBuilder/src/main/java/smallville7123/views/TextBook.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class TextBook {
+    String TAG = "TextBook";
     CharSequence text;
     Rect bounds = new Rect();
     int canvasWidth;
@@ -34,6 +35,7 @@ public class TextBook {
     public void draw(Canvas canvas, TextPaint textPaint) {
         char[] text = toArray(this.text);
         BufferedReader reader = new BufferedReader(new CharArrayReader(text, 0, text.length));
+        canvasWidth = canvas.getWidth();
         try {
             String line = reader.readLine();
             while (line != null) {
@@ -46,73 +48,64 @@ public class TextBook {
     }
 
     private void drawLine(Canvas canvas, String line, TextPaint textPaint) {
-        canvasWidth = canvas.getWidth();
+        drawLine(canvas, line, textPaint, null);
+    }
+
+    class TextStats {
+
+    }
+
+    private void drawLine(Canvas canvas, String line, TextPaint textPaint, TextStats textStats) {
+        // mays as well obtain the bounds of the entire text
         int characters = line.length();
         textPaint.getTextBounds(line, 0, characters, bounds);
-        int i = 0;
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
-        assertBounds(line, 0, ++i, textPaint);
+        if (bounds.width() > canvasWidth) {
 
-//        drawLine(canvas, line, textPaint, textBoundsWidth > canvasWidth);
-    }
-
-    private void assertBounds(String line, int start, int end, TextPaint textPaint) {
-        int textBoundsWidth = 0;
-        int currentWidth = 0;
-        String sample = line.substring(start, end);
-        textBoundsWidth += textPaint.measureText(sample, 0, sample.length());
-        for (int i = 0; i < sample.length();) {
-            currentWidth += textPaint.measureText(sample, i, ++i);
-        }
-
-        if (currentWidth != textBoundsWidth) {
-            // width mismatch: expected: 6864, received: 6875
-            throw new RuntimeException(
-                    "sample: " + Arrays.toString(toArray(sample)) + ", width mismatch: expected: " + textBoundsWidth + ", received: " + currentWidth
-            );
-        }
-        Log.d("TextBook", "sample: " + Arrays.toString(toArray(sample)) + ", assertBounds() width matches: expected: " + textBoundsWidth + ", received: " + currentWidth);
-    }
-
-    private void drawLine(Canvas canvas, String line, TextPaint textPaint, boolean wrap) {
-        if (wrap) {
             // bounds.width() exceeds canvas width
             // process each character individually
-            // do not draw each character, instead build up a string, then draw it
-            int characters = line.length();
+            // we do not draw each character, but instead build up a string, and then draw it
+
             char[] chars = new char[characters];
+            float[] widths = new float[characters];
+
+            // get our widths for each character
+            //
+            // NOTE: using measureText for each character produces incorrect widths
+            //
+            // while getTextWidths for the entire string produces correct widths
+            //
+
+            textPaint.getTextWidths(line, widths);
+
+            // TODO: getTextWidths returns a float array, however canvas.getWidth returns an int
+            //  how do we account for this?
+            //  fow now, just store the canvas width as float
+
             int charCount = 0;
-            int currentWidth = 0;
-            int maxWidth = canvasWidth;
+            float currentWidth = -bounds.left;
+            float cw = canvasWidth;
             for (int i = 0; i < characters; i++) {
-                textPaint.getTextBounds(line, i, i+1, bounds);
-                int t = currentWidth + bounds.width();
-                if (t <= canvasWidth) {
-                    currentWidth = t;
+                float w = currentWidth + widths[i];
+                if (w <= cw) {
+                    currentWidth = w;
                     chars[i] = line.charAt(i);
                     charCount++;
                 } else {
                     break;
                 }
             }
-            // about 44 to 45
-            canvas.drawText(chars, 0, charCount++, -bounds.left, -bounds.top, textPaint);
+            Log.d(TAG, "charCount = [" + charCount + "]");
+            // the bounds of the new text may be different than the bounds of the old text
+            textPaint.getTextBounds(chars, 0, charCount, bounds);
+            int index = 0;
+            int count = charCount;
+            float x = -bounds.left;
+            float y = -bounds.top;
+            canvas.drawText(chars, index, count, x, y, textPaint);
         } else {
+            // bounds.width() does not exceed canvas width
             int start = 0;
-            int end = line.length();
+            int end = characters;
             float x = -bounds.left;
             float y = -bounds.top;
             canvas.drawText(line, start, end, x, y, textPaint);
