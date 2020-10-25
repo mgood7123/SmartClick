341b4f98d62f86a9c234e01efc8d610e5675aa3f
smallville7123 <smallville7123@gmail.com>
2020-10-25 22:34:30 +1000

    start to implement TextBookGL, will restore Java TextBook later

---
+++ TextBook/src/main/java/smallville7123/textbook/TextBookGL.java
@@ -0,0 +1,114 @@
+package smallville7123.textbook;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class TextBookGL extends SurfaceView implements SurfaceHolder.Callback2 {
+
+    static {
+        System.loadLibrary("TextBookGL");
+    }
+
+    native void nativeSetSurface(Surface surface);
+
+    /**
+     * An alternative to surfaceRedrawNeeded where it is not required to block
+     * until the redraw is complete. You should initiate the redraw, and return,
+     * later invoking drawingFinished when your redraw is complete.
+     * <p>
+     * This can be useful to avoid blocking your main application thread on rendering.
+     * <p>
+     * As of O, if this is implemented {@link #surfaceRedrawNeeded} will not be called.
+     * However it is still recommended to implement {@link #surfaceRedrawNeeded} for
+     * compatibility with older versions of the platform.
+     *
+     * @param holder          The SurfaceHolder which needs redrawing.
+     * @param drawingFinished A runnable to signal completion. This may be invoked
+     */
+    @Override
+    public void surfaceRedrawNeededAsync(SurfaceHolder holder, Runnable drawingFinished) {
+
+    }
+
+    /**
+     * Called when the application needs to redraw the content of its
+     * surface, after it is resized or for some other reason.  By not
+     * returning from here until the redraw is complete, you can ensure that
+     * the user will not see your surface in a bad state (at its new
+     * size before it has been correctly drawn that way).  This will
+     * typically be preceeded by a call to {@link #surfaceChanged}.
+     * <p>
+     * As of O, {@link #surfaceRedrawNeededAsync} may be implemented
+     * to provide a non-blocking implementation. If {@link #surfaceRedrawNeededAsync}
+     * is not implemented, then this will be called instead.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     */
+    @Override
+    public void surfaceRedrawNeeded(SurfaceHolder holder) {
+
+    }
+
+    public TextBookGL(Context context) {
+        super(context);
+    }
+
+    public TextBookGL(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextBookGL(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public TextBookGL(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /**
+     * This is called immediately after the surface is first created.
+     * Implementations of this should start up whatever rendering code
+     * they desire.  Note that only one thread can ever draw into
+     * a {@link Surface}, so you should not draw into the Surface here
+     * if your normal rendering will be in another thread.
+     *
+     * @param holder The SurfaceHolder whose surface is being created.
+     */
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        nativeSetSurface(holder.getSurface());
+    }
+
+    /**
+     * This is called immediately after any structural changes (format or
+     * size) have been made to the surface.  You should at this point update
+     * the imagery in the surface.  This method is always called at least
+     * once, after {@link #surfaceCreated}.
+     *
+     * @param holder The SurfaceHolder whose surface has changed.
+     * @param format The new PixelFormat of the surface.
+     * @param width  The new width of the surface.
+     * @param height The new height of the surface.
+     */
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    /**
+     * This is called immediately before a surface is being destroyed. After
+     * returning from this call, you should no longer try to access this
+     * surface.  If you have a rendering thread that directly accesses
+     * the surface, you must ensure that thread is no longer touching the
+     * Surface before returning from this function.
+     *
+     * @param holder The SurfaceHolder whose surface is being destroyed.
+     */
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        nativeSetSurface(null);
+    }
+}
---
+++ TextBook/src/main/jni/TextBookGL.cpp
@@ -0,0 +1,191 @@
+//
+// Created by matthew good on 25/10/20.
+//
+
+#include "SkiaInstance.h"
+#include "JniHelpers.h"
+#include "log.h"
+#include <thread>
+
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+
+#include <EGL/egl.h>
+#include <GLES/gl.h>
+
+static ANativeWindow *window = 0;
+
+class EGL {
+public:
+    int init_EGL = false;
+    bool
+            init_eglGetDisplay = false,
+            init_eglInitialize = false,
+            init_eglChooseConfig = false,
+            init_eglCreateWindowSurface = false,
+            init_eglCreatePbufferSurface = false,
+            init_eglCreateContext = false,
+            init_eglMakeCurrent = false,
+            init_debug = false;
+    const GLint
+            *configuration_attributes = nullptr,
+            *context_attributes = nullptr,
+            *surface_attributes = nullptr;
+    EGLint
+            eglMajVers = 0,
+            eglMinVers = 0,
+            number_of_configurations = 0;
+    EGLNativeDisplayType display_id = EGL_DEFAULT_DISPLAY;
+    EGLDisplay display = EGL_NO_DISPLAY;
+    EGLConfig configuration = 0;
+    EGLContext
+            context = EGL_NO_CONTEXT,
+            shared_context = EGL_NO_CONTEXT;
+
+    int EGL_CONTEXT_CLIENT_VERSION_ = 3;
+
+    EGLSurface surface = EGL_NO_SURFACE;
+    // previously: ANativeWindow *native_window = nullptr;
+    EGLNativeWindowType native_window = 0;
+    GLint
+            width = 0,
+            height = 0,
+            surface_width = 0,
+            surface_height = 0;
+
+    void basicInit() {
+        const EGLint config_attrs[] = {EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT, EGL_NONE};
+        configuration_attributes = config_attrs;
+
+        const EGLint surface_attrs[] = {EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT, EGL_BLUE_SIZE, 8,
+                                  EGL_GREEN_SIZE, 8, EGL_RED_SIZE, 8, EGL_ALPHA_SIZE, 8,
+                                  EGL_DEPTH_SIZE, 16, EGL_NONE};
+        surface_attributes = surface_attrs;
+
+        // do no error checking
+
+        LOG_INFO("Initializing");
+
+        eglBindAPI(EGL_OPENGL_ES_API);
+
+        LOG_INFO("Initializing display");
+        display = eglGetDisplay(display_id);
+        init_eglGetDisplay = true;
+
+        eglInitialize(display, &eglMajVers, &eglMinVers);
+        init_eglInitialize = true;
+        LOG_INFO("Initialized display");
+
+        LOG_INFO("Initializing configuration");
+
+        const EGLint context_attrs[] = {EGL_CONTEXT_CLIENT_VERSION, EGL_CONTEXT_CLIENT_VERSION_,
+                                        EGL_NONE};
+        context_attributes = context_attrs;
+
+        eglChooseConfig(display, configuration_attributes, &configuration, 1,
+                        &number_of_configurations);
+        init_eglChooseConfig = true;
+        LOG_INFO("Initialized configuration");
+
+        LOG_INFO("Initializing surface");
+        surface = eglCreateWindowSurface(display, configuration, native_window,
+                                             nullptr);
+        init_eglCreateWindowSurface = true;
+        LOG_INFO("Initialized surface");
+
+        LOG_INFO("Initializing context");
+        context = eglCreateContext(display, configuration, shared_context,
+                                       context_attributes);
+        init_eglCreateContext = true;
+        LOG_INFO("Initialized context");
+
+        LOG_INFO("Switching to context");
+        eglMakeCurrent(display, surface, surface, context);
+        init_eglMakeCurrent = true;
+        LOG_INFO("Switched to context");
+
+        LOG_INFO("Obtaining surface width and height");
+        eglQuerySurface(display, surface, EGL_WIDTH, &surface_width);
+        eglQuerySurface(display, surface, EGL_HEIGHT, &surface_height);
+        LOG_INFO("Obtained surface width and height");
+
+        init_EGL = true;
+        LOG_INFO("Initialized");
+    };
+
+    void basicDenit() {
+        if (!init_EGL) return;
+
+        LOG_INFO("Uninitializing");
+
+        if (init_eglMakeCurrent) {
+            LOG_INFO("Switching context to no context");
+            eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+            init_eglMakeCurrent = false;
+        }
+        if (init_eglCreateContext) {
+            LOG_INFO("Uninitializing context");
+            eglDestroyContext(display, context);
+            context = EGL_NO_CONTEXT;
+            shared_context = EGL_NO_CONTEXT;
+            init_eglCreateContext = false;
+        }
+        if (init_eglCreateWindowSurface || init_eglCreatePbufferSurface) {
+            LOG_INFO("Uninitializing surface");
+            eglDestroySurface(display, surface);
+            surface = EGL_NO_SURFACE;
+            init_eglCreateWindowSurface = false;
+            init_eglCreatePbufferSurface = false;
+        }
+
+        if (init_eglInitialize) {
+            LOG_INFO("Uninitializing display");
+            eglTerminate(display);
+            init_eglInitialize = false;
+        }
+
+        if (init_eglGetDisplay) {
+            LOG_INFO("Setting display to no display");
+            display = EGL_NO_DISPLAY;
+            init_eglGetDisplay = false;
+        }
+
+        init_EGL = false;
+        LOG_INFO("Uninitialized");
+    }
+};
+
+class TextBookGL {
+public:
+    class EGL egl;
+
+    void setWindow(ANativeWindow *pWindow) {
+        egl.native_window = pWindow;
+        if (egl.native_window == 0) {
+            egl.basicDenit();
+        } else {
+            egl.basicInit();
+        }
+    }
+
+    void loop() {
+
+    }
+};
+
+TextBookGL textBookGL = TextBookGL();
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_smallville7123_textbook_TextBookGL_nativeSetSurface(JNIEnv *env, jobject thiz,
+                                                         jobject surface) {
+    if (surface != 0) {
+        window = ANativeWindow_fromSurface(env, surface);
+        LOG_INFO("Got window %p", window);
+        textBookGL.setWindow(window);
+    } else {
+        LOG_INFO("Releasing window");
+        ANativeWindow_release(window);
+        textBookGL.setWindow(0);
+    }
+}
